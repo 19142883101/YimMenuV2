00e20375162e491f14852f1c777de719d39a450c
He_Tong_xue <19142883101@163.com>
2025-07-26 23:33:00 +0800

    Update ClearDamage.cpp

--- src/game/features/self/ClearDamage.cpp
+++ src/game/features/self/ClearDamage.cpp
@@ -13,5 +13,5 @@ namespace YimMenu::Features
 		}
 	};
 
-	static ClearDamage _ClearDamage{"cleardamage", "Clear Damage", "Clears all the dirt and blood on you."};
+	static ClearDamage _ClearDamage{"cleardamage", "清除损坏", "清除你身上所有的污垢和血迹。"};
 }
