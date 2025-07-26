39afae96b4d966f6c0cae5e685763f20cc111572
He_Tong_xue <19142883101@163.com>
2025-07-26 23:21:25 +0800

    Update SpamKillfeed.cpp

--- src/game/features/network/SpamKillfeed.cpp
+++ src/game/features/network/SpamKillfeed.cpp
@@ -55,5 +55,5 @@ namespace YimMenu::Features
 		}
 	};
 
-	static SpamKillfeed _SpamKillfeed{"spamkillfeed", "Spam Killfeed", "Spams the killfeed of all players with fake kill entries. Use with caution"};
+	static SpamKillfeed _SpamKillfeed{"spamkillfeed", "刷屏击杀提示", "向所有玩家的击杀提示栏发送虚假击杀信息，谨慎使用"};
 }
